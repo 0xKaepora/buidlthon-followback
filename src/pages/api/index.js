39a7b02782d71a398880c77200f8549f266e0ff8
0xKaepora <0xkaeporagaebora@proton.me>
2024-03-01 18:17:08 -0700

    init

---
+++ src/pages/api/index.js
@@ -0,0 +1,225 @@
+// pages/api/og/index.js
+import satori from "satori";
+import fs from "fs";
+import sharp from "sharp";
+import path from "path";
+
+export async function generateErrorImage(heading, subheading) {
+  const imageUrl = "https://degen-frame.vercel.app/confused.png";
+
+  const svg = await satori(
+    `<svg width="1910" height="1000" xmlns="http://www.w3.org/2000/svg">
+      <img
+        src="${imageUrl}"
+        alt="error bg-image"
+        style="position: absolute; top: 0; left: 0"
+      />
+      <div
+        xmlns="http://www.w3.org/1999/xhtml"
+        style="display: flex; flexDirection: column; justifyContent: center; alignItems: center; color: white; fontFamily: Inter; width: 1910px; height: 1000px;"
+      >
+        <p style="fontSize: 128px; color: #38BDF8;">
+          ${heading}
+        </p>
+        <p style="fontSize: 84px; color: #CBD5E1;">
+          ${subheading}
+        </p>
+      </div>
+    </svg>`,
+    {
+      width: 1910,
+      height: 1000,
+      fonts: [
+        {
+          name: "Inter",
+          data: fs.readFileSync(
+            path.resolve(process.cwd(), "src/fonts", "Inter.ttf")
+          ),
+          weight: 400,
+          style: "normal",
+        },
+      ],
+    }
+  );
+
+  // Convert SVG to PNG using sharp
+  const png = await sharp(Buffer.from(svg)).png().toBuffer();
+
+  return png;
+}
+
+export async function generateImage(walletAddress) {
+  // Load the Inter font file
+  const interFontBuffer = fs.readFileSync(
+    path.resolve(process.cwd(), "src/fonts", "Inter.ttf")
+  );
+
+  const transactionsData = await getTransactionsData(walletAddress);
+  const currentPrice = await getCurrentPrice();
+  const gainLossUsd = (
+    currentPrice * transactionsData.totalQuantity -
+    transactionsData.totalUsdValue
+  ).toFixed(2);
+  const gainLossPercentage = (
+    (gainLossUsd / transactionsData.totalUsdValue) *
+    100
+  ).toFixed(2);
+
+  let name = `${walletAddress.substring(0, 6)}...${walletAddress.substring(
+    walletAddress.length - 4
+  )}`;
+
+  try {
+    const { ens, avatar_url } = await getEnsData(walletAddress);
+    if (ens) {
+      name = ens;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+
+  const formattedGainLossUsd = parseFloat(gainLossUsd).toLocaleString("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+
+  let imageUrl;
+  let message;
+  let subMessage;
+
+  if (gainLossUsd > 0) {
+    imageUrl = "https://degen-frame.vercel.app/happy.png";
+    message = `${formattedGainLossUsd}`;
+    subMessage = `${Math.round(gainLossPercentage)}%`;
+  } else if (gainLossUsd < 0) {
+    imageUrl = "https://degen-frame.vercel.app/sad.png";
+    message = `${formattedGainLossUsd}`;
+    subMessage = `${Math.round(gainLossPercentage)}%`;
+  } else {
+    imageUrl = "https://degen-frame.vercel.app/cry.png";
+    message = "NO DEGEN?!";
+    subMessage = "click below to buy some";
+  }
+
+  const svg = await satori(
+    <svg width="1910" height="1000" xmlns="http://www.w3.org/2000/svg">
+      <img
+        src={imageUrl}
+        alt="happy or sad bg-image"
+        style={{ position: "absolute", top: 0, left: 0 }}
+      />
+      <div
+        xmlns="http://www.w3.org/1999/xhtml"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "white",
+          fontFamily: "Inter", // Using the Inter font
+          width: "1910px",
+          height: "1000px",
+        }}
+      >
+        <p
+          style={{
+            marginBottom: "20px",
+            fontSize: "84px",
+            color: "#CBD5E1",
+          }}
+        >
+          {name}
+        </p>
+        <p style={{ fontSize: "128px", color: "#38BDF8" }}>{message}</p>
+        <p style={{ fontSize: "84px", color: "#CBD5E1" }}>{subMessage}</p>
+      </div>
+    </svg>,
+    {
+      width: 1910,
+      height: 1000,
+      fonts: [
+        {
+          name: "Inter",
+          data: interFontBuffer,
+          weight: 400,
+          style: "normal",
+        },
+      ],
+    }
+  );
+
+  // Convert SVG to PNG using sharp
+  const png = await sharp(Buffer.from(svg)).png().toBuffer();
+
+  return png;
+}
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    // Handle POST request
+    const { untrustedData } = req.body;
+    console.log(req.body);
+    if (untrustedData && untrustedData.fid) {
+      const fid = untrustedData.fid;
+      console.log(fid);
+      // Make a request to the API
+      const response = await fetch(
+        `https://searchcaster.xyz/api/profiles?fid=${fid}`
+      );
+      const data = await response.json();
+
+      // Store the connectedAddress
+      let connectedAddress = null;
+
+      try {
+        connectedAddress = data[0].connectedAddress;
+      } catch (error) {
+        console.error(error);
+      }
+
+      //static degen addr
+      const tokenAddress = "0x4ed4e862860bed51a9570b96d89af5e1b0efefed";
+
+      try {
+        res.status(200).send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta name="fc:frame" content="vNext" />
+            <meta name="fc:frame:image" content="https://degen-frame.vercel.app/api/og/${connectedAddress}" />
+            <meta name="fc:frame:button:1" content="buy MOAR $degen 🎩" />
+            <meta name="fc:frame:button:1:action" content="post_redirect" />
+            <meta
+            name="fc:frame:post_url"
+              content="https://degen-frame.vercel.app/api/uniswap/${tokenAddress}"
+            />
+          </head>
+        </html>
+      `);
+      } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+    } else {
+      return res.status(400).json({ error: "No data received" });
+    }
+  } else if (req.method === "GET") {
+    const { walletAddress } = req.query;
+
+    // Proceed only if walletAddress is provided
+    if (!walletAddress) {
+      return res.status(400).json({ error: "Wallet address is required" });
+    }
+
+    try {
+      const svg = await generateImage(walletAddress);
+      res.setHeader("Content-Type", "image/png");
+      res.send(svg);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    // Handle other HTTP methods or return an error
+    res.status(405).send({ error: "Method Not Allowed" });
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,118 @@
+import Head from "next/head";
+import { Inter } from "next/font/google";
+// import styles from "@/styles/Home.module.css";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const MarkdownContent = styled.div`
+  display: flex; /* Use flexbox to center children */
+  justify-content: center; /* Center children horizontally */
+  align-items: center; /* Center children vertically */
+  min-height: 100vh; /* Minimum height of the viewport */
+  padding: 20px;
+  background-color: #f0f0f0;
+  color: #1a1a1a;
+  border-radius: 8px;
+  font-family: Arial, sans-serif;
+  line-height: 1.6;
+  background-color: #f0f0f0; /* Lighter background */
+  color: #1a1a1a; /* Darker text color */
+  padding: 20px;
+  border-radius: 8px;
+  font-family: Arial, sans-serif;
+  line-height: 1.6;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: #333; /* Even darker color for headers */
+    margin-top: 24px;
+    margin-bottom: 16px;
+    font-weight: 600;
+  }
+
+  p {
+    margin-top: 0;
+    margin-bottom: 16px;
+  }
+
+  a {
+    color: #0366d6;
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  code {
+    padding: 0.2em 0.4em;
+    margin: 0;
+    font-size: 90%;
+    background-color: rgba(27, 31, 35, 0.1);
+    border-radius: 6px;
+  }
+
+  pre {
+    padding: 16px;
+    overflow: auto;
+    font-size: 90%;
+    line-height: 1.45;
+    background-color: #f6f8fa;
+    border-radius: 6px;
+  }
+
+  /* Add more styles as needed */
+`;
+
+export default function Home() {
+  const [readmeContent, setReadmeContent] = useState("");
+
+  // Fetch README content from GitHub
+  useEffect(() => {
+    fetch(
+      // "https://raw.githubusercontent.com/KartikC/degen-frame/main/README.md"
+      "https://raw.githubusercontent.com/0xKaepora/buidlthon-followback/main/README.md"
+    )
+      .then((response) => response.text())
+      .then((text) => {
+        setReadmeContent(text);
+      })
+      .catch((error) => console.error("Error fetching README:", error));
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>BUIDLathon :: Denver 2024</title>
+        <meta name="description" content="Please just work" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="fc:frame" content="vNext" />
+        <meta
+          name="fc:frame:image"
+          content="https://degen-frame.vercel.app/default.png"
+        />
+        <meta property="og:image" content="/default.png" />
+        <meta name="fc:frame:button:1" content="Reveal my degeneracy 🎩" />
+        <meta
+          name="fc:frame:post_url"
+          content="https://degen-frame.vercel.app/api/"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <MarkdownContent>
+          <div style={{ maxWidth: "800px", width: "100%" }}>
+            <ReactMarkdown>{readmeContent}</ReactMarkdown>
+          </div>
+        </MarkdownContent>
+      </main>
+    </>
+  );
+}
